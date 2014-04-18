401c322b309dff17c4f7fe6553cdec26fd4d78d4
sfarris <sfarris@mines.edu>
2014-04-17 20:19:50 -0600

    made small progress on infoPanel

--- src/gui/InfoBar.java
+++ src/gui/InfoBar.java
@@ -2,6 +2,7 @@ package gui;
 
 import java.util.ArrayList;
 
+import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
 import javax.swing.table.TableColumn;
@@ -10,27 +11,35 @@ public class InfoBar extends JTabbedPane{
 	private static final long serialVersionUID = 1L;
 
 	InfoBar(){
-		this.addTab("Markers", new MarkerTable());
+		String[] columnNames = {"First Name",
+				"Last Name",
+				"Sport",
+				"# of Years",
+		"Vegetarian"};
+
+		Object[][] data = {{"Mary", "Campione","Snowboarding", new Integer(5), new Boolean(false)},
+							{"Alison", "Huml","Rowing", new Integer(3), new Boolean(true)},
+							{"Kathy", "Walrath","Knitting", new Integer(2), new Boolean(false)},
+							{"Sharon", "Zakhour","Speed reading", new Integer(20), new Boolean(true)},
+							{"Philip", "Milne","Pool", new Integer(10), new Boolean(false)}};
+
+		JTable markerTable = new JTable(data, columnNames);
+		JScrollPane scrollPane = new JScrollPane(markerTable);
+		this.addTab("Markers", markerTable);
 		this.addTab("Teams", new TeamTable());
 	}
-	
+
 	public class MarkerTable extends JTable{
 		private static final long serialVersionUID = 1L;
 		MarkerTable(){
-			String[] markers = new String [3];
-			markers[0] = "hello";
-			markers[1] = "espen";
-			markers[2] = "test";
-			TableColumn test = new TableColumn();
-			test.setIdentifier("test");
-			this.addColumn(test);
+			
 		}
 	}
-	
+
 	public class TeamTable extends JTable{
 		private static final long serialVersionUID = 1L;
 		TeamTable(){
-			
+			TableColumn nameColumn = new TableColumn();
 		}
 	}
 }
